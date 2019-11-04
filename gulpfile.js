eeb053022a1c256828c263b1b036520c52fdc7bf
Egaro555 <hugues.vogel@hotmail.fr>
2019-11-04 02:57:24 +0100

    feat(nav): create html navbar

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var connect = require('gulp-connect');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
-var servePort = 4000;
-var fs = require("fs");
-var open = require('gulp-open');
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const connect = require('gulp-connect');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+const servePort = 4000;
+const fs = require("fs");
+const open = require('gulp-open');
+const sass = require('gulp-sass');
+const bodyParser = require('body-parser')
 
 gulp.task('build-ts', function () {
     return tsProject.src()
@@ -16,11 +18,12 @@ gulp.task('copy-html', function () {
     return gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist/'));
 });
-gulp.task('copy-css', function () {
-    return gulp.src('./src/**/*.css')
-        .pipe(gulp.dest('./dist/'));
+gulp.task('build-sass', function () {
+    return gulp.src('./src/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
 });
-gulp.task('build', gulp.parallel('build-ts', 'copy-html', 'copy-css'));
+gulp.task('build', gulp.parallel('build-ts', 'copy-html', 'build-sass'));
 gulp.task('build-watch', gulp.series('build', function () {
     return watch('src/**', function () {
         console.log('watch detection');
@@ -29,19 +32,75 @@ gulp.task('build-watch', gulp.series('build', function () {
 }));
 gulp.task('open-browser', function () {
     gulp.src(__filename)
-        .pipe(open({uri: 'http://localhost:'+servePort}));
+        .pipe(open({uri: 'http://localhost:' + servePort}));
 });
-gulp.task('serve-start', function() {
-    var express = require('express');
-    var app = express();
-    app.get('/decoders', function(req, res) {
-        res.send(fs.readdirSync(__dirname+'/dist/decoders'));
+gulp.task('serve-start', function () {
+    const express = require('express');
+    const app = express();
+
+    app.use('/src', express.static(__dirname + '/src'));
+    app.use(bodyParser.text());
+
+    app.get('/decoders', function (req, res) {
+        res.send(fs.readdirSync(__dirname + '/src/decoders'));
+    });
+    app.patch('/src/decoders/rename/:fileName/:newFileName', function (req, res) {
+        const from = __dirname + '/src/decoders/' + req.params.fileName;
+        const to = __dirname + '/src/decoders/' + req.params.newFileName;
+        if (!fs.existsSync(from)) {
+            res.send(404, 'Script not found');
+            return;
+        }
+        if(fs.existsSync(to)){
+            res.send(409, 'Script already exist');
+            return;
+        }
+
+        fs.renameSync(from, to);
+        res.send('Rename done');
+
+    });
+    app.post('/src/decoders/:fileName', function (req, res) {
+        const file = __dirname + '/src/decoders/' + req.params.fileName;
+        const content = req.body;
+
+        if (fs.existsSync(file)) {
+            fs.writeFileSync(file, content, 'utf8');
+        } else {
+            res.send(404, 'Script not found');
+            return;
+        }
+        res.send('saved');
+    });
+    app.put('/src/decoders/:fileName', function (req, res) {
+        const file = __dirname + '/src/decoders/' + req.params.fileName;
+        const content = req.body;
+
+        if (!fs.existsSync(file)) {
+            fs.writeFileSync(file, content, 'utf8');
+        } else {
+            res.send(409, 'Script already exist');
+            return;
+        }
+        res.send('saved');
+    });
+    app.delete('/src/decoders/:fileName', function (req, res) {
+        const file = __dirname + '/src/decoders/' + req.params.fileName;
+
+        if (fs.existsSync(file)) {
+            fs.unlinkSync(file);
+        } else {
+            res.send(404, 'Script not found');
+            return;
+        }
+
+        res.send('deleted');
     });
 
     return connect.server({
         root: 'dist',
         port: servePort,
-        middleware: function(connect, opt) {
+        middleware: function (connect, opt) {
             return [app];
         }
     })
---
+++ src/script.ts
@@ -0,0 +1,213 @@
+declare const Swal: any;
+
+interface Decoder {
+	type: 'ts' | 'js';
+	fileName: string;
+	navHtmlElement?: HTMLElement;
+}
+
+interface DecoderSource {
+	js: string;
+	ts?: string;
+}
+
+$(() => {
+	let decoders: Decoder[];
+	let currentDecoder: Decoder;
+	const noop = () => undefined;
+
+	const defaultJsDecoderContent = ``;
+	const defaultTsDecoderContent = ``;
+
+	async function deleteDecoder(decoder: Decoder): Promise<void> {
+
+		const fileName = decoder.fileName + '.' + decoder.type;
+
+		await $.ajax({method: 'DELETE', url: `/src/decoders/${fileName}`});
+
+		$(decoder.navHtmlElement).remove();
+		decoders.splice(decoders.indexOf(decoder),1);
+	}
+
+	async function renameDecoder(decoder: Decoder, newName: string): Promise<void> {
+
+		const fileName = decoder.fileName + '.' + decoder.type;
+		const newFileName = newName + '.' + decoder.type;
+
+		await $.ajax({method: 'PATCH', url: `/src/decoders/rename/${fileName}/${newFileName}`});
+
+		decoder.fileName = newName;
+		$(decoder.navHtmlElement).find('.fileName').text(newName);
+	}
+
+	async function copyDecoder(decoder: Decoder, newName: string) {
+
+		const fileName = decoder.fileName + '.' + decoder.type;
+		const newFileName = newName + '.' + decoder.type;
+
+		const source = await getSource(decoder);
+
+		await $.ajax({
+			method: 'PUT',
+			url: `/src/decoders/${newFileName}`,
+			data: source,
+			processData: false,
+			dataType: 'text',
+			contentType: 'text/plain',
+		});
+
+		const newDecoder: Decoder = {
+			fileName: newName,
+			type: decoder.type
+		};
+
+		decoders.push(newDecoder);
+		addDecoderToList(newDecoder);
+	}
+
+	async function uiDeleteDecoder(decoder: Decoder): Promise<void> {
+
+		const {value: isConfirm} = await Swal.fire({
+			title: 'Etes vous sur de vouloir supprimer le decoder ' + decoder.fileName,
+			type: 'warning',
+			showCancelButton: true,
+			focusConfirm: true,
+			confirmButtonText: 'Delete',
+			cancelButtonText: 'Annuler',
+		});
+
+		if (isConfirm) {
+			await deleteDecoder(decoder);
+		}
+	}
+
+	async function uiRenameDecoder(decoder: Decoder): Promise<void> {
+		const {value: newFileName} = await Swal.fire({
+			title: 'Nouveau nom',
+			input: 'text',
+			inputValue: decoder.fileName,
+			showCancelButton: true,
+			inputValidator: (value) => {
+				if (!value) {
+					return 'Le nom ne doit pas étre vide'
+				} else if (decoder.fileName !== value && decoders.some((d) => d.fileName === value)) {
+					return 'Ce nom a déja étais utiliser';
+				}
+			}
+		});
+
+		if (newFileName && newFileName !== decoder.fileName) {
+			await renameDecoder(decoder, newFileName);
+		}
+	}
+
+	async function uiCopyDecoder(decoder: Decoder): Promise<void> {
+		const {value: newFileName} = await Swal.fire({
+			title: 'Nom de la copy',
+			input: 'text',
+			inputValue: decoder.fileName + '_copy',
+			showCancelButton: true,
+			inputValidator: (value) => {
+				if (!value) {
+					return 'Le nom ne doit pas étre vide'
+				} else if (decoders.some((d) => d.fileName === value)) {
+					return 'Ce nom a déja étais utiliser';
+				}
+			}
+		});
+
+		if (newFileName) {
+			await copyDecoder(decoder, newFileName);
+		}
+	}
+
+	async function getSource(decoder: Decoder): Promise<string> {
+		return $.get(`/src/decoders/${decoder.fileName}.${decoder.type}`)
+	}
+
+	function selectDecoder(decoder: Decoder): void {
+		if (currentDecoder) {
+			$(currentDecoder.navHtmlElement).removeClass('active')
+		}
+		$(decoder.navHtmlElement).addClass('active');
+		currentDecoder = decoder;
+	}
+
+	async function newDecoder() {
+		const inputName = $('#add-decoder-name');
+		const inputType = $('#add-decoder-type');
+		const decoder: Decoder = {
+			type: inputType.val() as 'js' | 'ts',
+			fileName: inputName.val() as string
+		};
+
+		await $.ajax({
+			type: 'PUT', url: `/src/decoders/${decoder.fileName}.${decoder.type}`,
+			data: decoder.type === 'js' ? defaultJsDecoderContent : defaultTsDecoderContent,
+			processData: false,
+			dataType: 'text',
+			contentType: 'text/plain',
+		});
+
+		decoders.push(decoder);
+		addDecoderToList(decoder);
+		inputName.val('');
+	}
+
+	function addDecoderToList(decoder: Decoder) {
+
+		const decodersContenaire = $('#decoders-list')[0];
+		const domBtn = $(`
+<button class="btn btn-script">
+	<span class="fileName">${decoder.fileName}</span>
+	<span class="badge badge-${decoder.type === 'ts' ? 'info' : 'warning'}">${decoder.type === 'ts' ? 'TypeScript' : 'JavaScript'}</span>
+	<a title="Renomer" class="material-icons float-right rename">edit</a>
+	<a title="Copyer" class="material-icons float-right copy">file_copy</a>
+	<a title="Suprimer" class="material-icons float-right delete">delete</a>
+</button>`);
+		domBtn.on('click', () => selectDecoder(decoder));
+		domBtn.find('.rename').on('click', (event) => {
+			event.stopPropagation();
+			uiRenameDecoder(decoder)
+		});
+		domBtn.find('.delete').on('click', (event) => {
+			event.stopPropagation();
+			uiDeleteDecoder(decoder)
+		});
+		domBtn.find('.copy').on('click', (event) => {
+			event.stopPropagation();
+			uiCopyDecoder(decoder)
+		});
+		decoder.navHtmlElement = domBtn[0];
+		decodersContenaire.append(decoder.navHtmlElement);
+	}
+
+	async function init() {
+		const decodersFileName: string[] = await $.get('decoders');
+		decoders = decodersFileName
+			.map((file) => /^(.*)\.(js|ts)$/.exec(file))
+			.filter(_ => _)
+			.map(regExpGroups => ({
+				fileName: regExpGroups[1],
+				type: regExpGroups[2]
+			} as Decoder));
+		decoders.forEach((decoder) => addDecoderToList(decoder));
+
+		$('#decoders form').on('submit', (e) => {
+			e.preventDefault();
+			newDecoder();
+		})
+		$('#decoders form select').on('change', (e) => newDecoder());
+	}
+
+	init().then(noop)
+
+	$('[propageFocus]')
+		.on('focus', function () {
+			$(this).parents().addClass('focused');
+		})
+		.on('blur', function () {
+			$(this).parents().removeClass('focused');
+		});
+
+});
